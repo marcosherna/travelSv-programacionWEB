0d6c10af0de33b04e0191865c18c2c5c91552e47
Marcos <marcosxdf6@gmail.com>
2023-06-14 17:39:18 +0200

    animaciones y home

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -2,10 +2,19 @@ const sectionMision = document.getElementById('section-mision')
 const sectionVision = document.getElementById('section-vision')
 const btnSectionAbout = document.querySelectorAll('.btn-section-about');
 
+const home = document.querySelector('#Home');
+const buttons = document.querySelectorAll('.btn-carousel-home')
+
+const loaderImg = document.querySelectorAll('.loader-img > span')
+const loader = document.querySelector('.loader-img');
+const indicatorItemCarousel = document.querySelectorAll('.indicator-item');
+
 const contactSection = document.getElementById('Contactanos');
 const contactForm = document.querySelector('.container-contact');
 
 const destinosSection = document.getElementById('Destinos');
+
+
 
 const animateElements = (entries, className) => {
     entries.forEach((entry) => {
@@ -40,7 +49,7 @@ const animateElements = (entries, className) => {
   const animationContactSection = (entries) => {
     animateElements(entries, 'container-contact');
   };
-  
+
   createAnimationObserver(animationDestinySection, destinosSection, 'fade-in');
   createAnimationObserver(animationContactSection, contactSection, 'container-contact');
   
@@ -59,3 +68,68 @@ btnSectionAbout.forEach(btn => {
         }
     })
 })
+
+
+
+const imagenes =  [];
+indicatorItemCarousel[0].style.backgroundColor = 'rgba(17, 17, 17, .5)';
+
+const activeLoader = ( ()=> {
+    loader.style.display = 'flex'
+    for (const span of loader.children) {
+        span.classList.add('loading');   
+        
+        setTimeout( ()=>{ 
+            span.classList.remove('loading');
+            loader.style.display = 'none';
+        }, 1500);
+    }
+});
+
+/* Prototipo para el carusel de imagenes*/
+let selectedItemCarousel = 0;
+const addImgCarousel = ()=>{
+    for(let i = 1;i <= 5; i++){
+        let img = new Image();
+        img.src = `./img/img-${i}.png`;
+        imagenes.push(img);
+    }
+    home.style.backgroundImage = `url(${imagenes[selectedItemCarousel].src})`;
+}
+
+const checkIndicatorCarousel = (index, length) => {
+    const indicator = indicatorItemCarousel[index];
+    indicator.style.backgroundColor ='#FFBC11';
+
+    indicatorItemCarousel[( index != 0)? index-1 : length-1]
+        .style.backgroundColor = 'rgba(17, 17, 17, .5)';
+
+
+   indicatorItemCarousel[( index != length-1)? index+1 : 0]
+        .style.backgroundColor = 'rgba(17, 17, 17, .5)';
+}
+
+addImgCarousel();
+buttons.forEach( btn => {
+
+    btn.addEventListener('click', function () {
+        if(this.getAttribute('id') == 'next'){
+            selectedItemCarousel = (selectedItemCarousel == imagenes.length-1) ? 
+                            0 : selectedItemCarousel+1;
+
+            home.style.backgroundImage = `url(${imagenes[selectedItemCarousel].src})`;
+            
+        } else {
+            selectedItemCarousel = (selectedItemCarousel == 0) ? 
+                            imagenes.length : selectedItemCarousel;
+
+            selectedItemCarousel--;
+            home.style.backgroundImage = `url(${imagenes[selectedItemCarousel].src})`;
+        }
+        checkIndicatorCarousel(selectedItemCarousel, imagenes.length);
+        activeLoader();
+    })
+});
+
+
+
